c1b7a733081163f1dc352980af14ef5a7b27f22f
momose1031 <macbookair2020momose@icloud.com>
2021-05-03 02:35:49 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '553b27d5d91e102f0a1160fb60fd5c1700057e2d28c18f8502254d9cab33c3f55b221fd872c8138795b620b2f854522ace378ca088922ecae92704fb06f94d03'
