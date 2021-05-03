a865be865a3635f3c5895cdec225fa2b8d620222
momose1031 <macbookair2020momose@icloud.com>
2021-05-03 00:36:51 +0000

    [finish]4-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permit_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permit_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
